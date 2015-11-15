5940d60256273edee35b9efcdeb710d5b5e91936
Paul Zabelin <pzabelin+git@gmail.com>
2015-11-14 21:25:34 -0800

    add sample xcode project

---
+++ Classes/SmartExceptionBreakpoint.h
@@ -0,0 +1,13 @@
+//
+//  SmartExceptionBreakpoint.h
+//  SmartExceptionBreakpoint
+//
+//  Created by Paul Zabelin on 11/14/15.
+//  Copyright © 2015 Paul Zabelin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SmartExceptionBreakpoint : NSObject
+
+@end
---
+++ Classes/SmartExceptionBreakpoint.m
@@ -0,0 +1,13 @@
+//
+//  SmartExceptionBreakpoint.m
+//  SmartExceptionBreakpoint
+//
+//  Created by Paul Zabelin on 11/14/15.
+//  Copyright © 2015 Paul Zabelin. All rights reserved.
+//
+
+#import "SmartExceptionBreakpoint.h"
+
+@implementation SmartExceptionBreakpoint
+
+@end
